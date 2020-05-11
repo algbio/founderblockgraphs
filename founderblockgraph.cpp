adbf6f1004cc6eec2ea3ab1f30a779aa69ecaec1
Tuukka Norri <tsnorri@iki.fi>
2020-05-11 19:43:57 +0300

    Replace std::size_t with size_type dependent on CST type
    – Apparently I gave bad advice on using std::size_t for storing nodes, since at least sdsl::cst_sada uses unsigned long long for those, and std::size_t is likely defined as unsigned long on my system.

--- founderblockgraph.cpp
+++ founderblockgraph.cpp
@@ -21,10 +21,15 @@ using namespace sdsl;
 /* Copyright (C) 2020 Veli Mäkinen under GNU General Public License v3.0 */
 
 
+typedef cst_sada<>          cst_type;
+typedef cst_type::node_type node_t;
+typedef cst_type::size_type size_type;
+
+
 // For debugging purposes naive string matching
-size_t count(string P,string T) {
-    size_t result = 0;
-    for (size_t i=0; i<T.size()-P.size(); i++) 
+size_type count(string P,string T) {
+    size_type result = 0;
+    for (size_type i=0; i<T.size()-P.size(); i++) 
         if (P==T.substr(i,P.size())) 
             result++;
     return result;  
@@ -36,7 +41,7 @@ int main(int argc, char **argv) {
         cout << "    This program constructs a founder block graph of MSA given in FASTA format" << endl;
         return 1;
     }
-    size_t GAPLIMIT=1; // Filtering out entries with too long gap regions
+    size_type GAPLIMIT=1; // Filtering out entries with too long gap regions
     if (argc>2)
         GAPLIMIT = atoi(argv[2]);
 
@@ -63,11 +68,11 @@ int main(int argc, char **argv) {
 
 
     // Filtering out entries with too many gaps or N's
-    size_t ngaps=0;
-    size_t maxgaprun=0;
-    size_t gaprun=0;
-    for (size_t i=0; i<MSAtemp.size(); i++) {
-        for (size_t j=0; j<MSAtemp[i].size(); j++)
+    size_type ngaps=0;
+    size_type maxgaprun=0;
+    size_type gaprun=0;
+    for (size_type i=0; i<MSAtemp.size(); i++) {
+        for (size_type j=0; j<MSAtemp[i].size(); j++)
             if (MSAtemp[i][j]=='-' || MSAtemp[i][j]=='N') {
                ngaps++;
                gaprun++;
@@ -88,8 +93,8 @@ int main(int argc, char **argv) {
     cout << "Input MSA[1.." << MSA.size() << ",1.." << MSA[0].size() << "]" << endl; 
     // Concatenating, removing gap symbols, and adding separators for indexing
     string C="";
-    for (size_t i=0; i<MSA.size(); i++) {
-        for (size_t j=0; j<MSA[i].size(); j++) 
+    for (size_type i=0; i<MSA.size(); i++) {
+        for (size_type j=0; j<MSA[i].size(); j++) 
             if (MSA[i][j]!='-') 
                 C += MSA[i][j]; 
         C += '#';
@@ -106,8 +111,7 @@ int main(int argc, char **argv) {
     
     string index_file   = string(argv[1])+plain_suffix+index_suffix;
 
-    typedef cst_sada<>::node_type node_t;
-    cst_sada<> cst;
+    cst_type cst;
     if (!load_from_file(cst, index_file)) {
         ifstream in(string(argv[1])+plain_suffix);
         if (!in) {
@@ -121,41 +125,41 @@ int main(int argc, char **argv) {
     cout << "Index construction complete, index requires " << size_in_mega_bytes(cst) << " MiB." << endl;
 
 
-    size_t n = MSA[0].size();
+    size_type n = MSA[0].size();
 
     /* v[j] is such that MSA[1..m][v[j]..j] is a repeat-free segment */
     /* The following computes it naively. 
        This is a preprocessing part of the main algorithm */
 
-    size_t *v = new size_t[n];
-    for (size_t i=0; i<n; i++)
+    size_type *v = new size_type[n];
+    for (size_type i=0; i<n; i++)
         v[i] = 0;
  
-    unordered_map<size_t, size_t> bwt2row;
+    unordered_map<size_type, size_type> bwt2row;
     /* bwt2row[j]=i maps j-th smallest suffix to the row of MSA */
 
-    size_t m = MSA.size();
-    size_t *sp = new size_t[m];
-    size_t *ep = new size_t[m];
-    size_t *lcp = new size_t[m];
+    size_type m = MSA.size();
+    size_type *sp = new size_type[m];
+    size_type *ep = new size_type[m];
+    size_type *lcp = new size_type[m];
     /* [sp[i]..ep[i]] maintains BWT interval of MSA[i][jp..j] */
     /* lcp[i] = j-jp+1 minus the number of gap symbols in that range*/
 
     // initializing
-    for (size_t i=0; i<m; i++) {
+    for (size_type i=0; i<m; i++) {
         sp[i] = 0;
         ep[i] = cst.csa.size()-1; 
         lcp[i] = 0;
     }   
 
-    size_t jp = n; // maintains the left-boundary 
-    size_t sum;
-    for (size_t j=n-1; j+1>0; j--) {
+    size_type jp = n; // maintains the left-boundary 
+    size_type sum;
+    for (size_type j=n-1; j+1>0; j--) {
         v[j] = j+1; // no valid range found
         if (j<n-1) {  
             /* contracting MSA[i][j] from right for all i */
             bwt2row.clear();
-            for (size_t i=0; i<m; i++) {
+            for (size_type i=0; i<m; i++) {
                 /* mapping BWT range to suffix tree node,
                    taking parent, mapping back if parent is not too far*/
                 // Keeping old range on gap symbols
@@ -187,7 +191,7 @@ int main(int argc, char **argv) {
             jp--; 
             /* Now looking at MSA[1..m][jp..j] */
             bwt2row.clear();
-            for (size_t i=0; i<m; i++) {
+            for (size_type i=0; i<m; i++) {
                 // Keeping old range on gap symbols
                 if (MSA[i][jp]!='-') {
                     sdsl::backward_search(cst.csa,sp[i],ep[i],MSA[i][jp],sp[i],ep[i]);
@@ -201,19 +205,19 @@ int main(int argc, char **argv) {
     /* Main algorithm begins */
     /* s[j] is the score of the optimal valid segmentation of MSA[1..m][1..j] */
     /* s[n]=min_{S is valid segmentation} max_{[a..b] \in S} b-a+1 */ 
-    size_t *s = new size_t[n];
+    size_type *s = new size_type[n];
 
     /* prev[j] is the pointer to the end of previous segment in optimal segmentation */
-    size_t *prev = new size_t[n];
+    size_type *prev = new size_type[n];
     // Computation of s[j]'s and prev[j]'s
-    for (size_t j=0; j<n; j++) {
+    for (size_type j=0; j<n; j++) {
         s[j] = n; // init
         prev[j] = n; // init
         if (v[j]>j) 
             continue; // no valid range
         s[j] = j+1; // handles case jp=0
         prev[j] = j+1; // handles case jp=0
-        for (size_t jp=v[j]; jp>0; jp--) {
+        for (size_type jp=v[j]; jp>0; jp--) {
             if (s[j]>max(s[jp-1],j-jp+1)) {
                 s[j] = max(s[jp-1],j-jp+1);
                 prev[j] = jp-1;       
@@ -225,14 +229,14 @@ int main(int argc, char **argv) {
     // outputing optimal score 
     cout << "Optimal score: " << s[n-1] << endl;
 
-    list<size_t> boundariestemp;
-    size_t j = n-1;
+    list<size_type> boundariestemp;
+    size_type j = n-1;
     boundariestemp.push_front(j);
     while (prev[j]<j) {
         boundariestemp.push_front(prev[j]);
         j = prev[j];         
     }
-    vector<size_t> boundaries;
+    vector<size_type> boundaries;
     for (const auto& j : boundariestemp)
         boundaries.push_back(j);
     cout << "Number of segments: " << boundaries.size() << endl;
@@ -241,12 +245,12 @@ int main(int argc, char **argv) {
     //    cout << boundary << endl;
 
     /* Convert segmentation into founder block graph */
-    unordered_map<string, size_t> str2id;
-    size_t nodecount = 0; 
-    size_t previndex = 0;
-    vector<size_t> *blocks = new vector<size_t>[boundaries.size()];
-    for (size_t j=0; j<boundaries.size(); j++) {
-        for (size_t i=0; i<m; i++)
+    unordered_map<string, size_type> str2id;
+    size_type nodecount = 0; 
+    size_type previndex = 0;
+    vector<size_type> *blocks = new vector<size_type>[boundaries.size()];
+    for (size_type j=0; j<boundaries.size(); j++) {
+        for (size_type i=0; i<m; i++)
             if (!str2id.count(MSA[i].substr(previndex,boundaries[j]-previndex+1))) {       
                 blocks[j].push_back(nodecount);
                 str2id[MSA[i].substr(previndex,boundaries[j]-previndex+1)] = nodecount++;
@@ -258,22 +262,22 @@ int main(int argc, char **argv) {
     for (const auto& pair : str2id) {
         labels[pair.second] = pair.first;
     }
-    size_t totallength = 0; 
-    for (size_t i=0; i<nodecount; i++)
+    size_type totallength = 0; 
+    for (size_type i=0; i<nodecount; i++)
         totallength += labels[i].size();
 
     cout << "#nodes=" << nodecount << endl;
     cout << "total length of node labels=" << totallength << endl; 
     
-    unordered_map<size_t, size_t> *edges = new unordered_map<size_t, size_t>[nodecount];
+    unordered_map<size_type, size_type> *edges = new unordered_map<size_type, size_type>[nodecount];
     previndex = 0;
-    for (size_t k=0; k<boundaries.size()-1; k++) {
-        for (size_t i=0; i<m; i++)
+    for (size_type k=0; k<boundaries.size()-1; k++) {
+        for (size_type i=0; i<m; i++)
             edges[str2id[MSA[i].substr(previndex,boundaries[k]-previndex+1)]][str2id[MSA[i].substr(previndex,boundaries[k+1]-boundaries[k]+1)]] = 1;
         previndex = boundaries[k]+1;
     }
-    size_t edgecount = 0;
-    for (size_t i=0; i<nodecount; i++)
+    size_type edgecount = 0;
+    for (size_type i=0; i<nodecount; i++)
         edgecount += edges[i].size();
     cout << "#edges=" << edgecount << endl;
 
